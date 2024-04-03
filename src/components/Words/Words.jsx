c34a8ddd840e3fd3373c33a01dcbe7e10a4d0d29
albertoG-2004 <alberto.gutierrezcetis138@gmail.com>
2024-04-02 21:28:20 -0600

    Modiying the words component, creating the function to check if the piece is in the correct place, designated area for the piece to appear

--- src/components/Words/Words.jsx
+++ src/components/Words/Words.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import Mentir from '../../img/Words/Mentir.png'
 import styles from './Words.module.css';
 
-export default function Words(){
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+export default function Words({image, targetArea}){
+    const area = {minX: -850, minY: 120, maxX: -100, maxY: 450 };
+    const initialPositionX = Math.random() * (area.maxX - area.minX) + area.minX;
+    const initialPositionY = Math.random() * (area.maxY - area.minY) + area.minY;
+    const [position, setPosition] = useState({ x: initialPositionX, y: initialPositionY });
     const [dragging, setDragging] = useState(false);
-
+    const [isInTargetArea, setIsInTargetArea] = useState(false);
+    
     const handleMouseDown = (e) => {
         setDragging(true);
         const initialX = e.clientX - position.x;
@@ -15,7 +18,18 @@ export default function Words(){
         if (dragging) {
             const newX = e.clientX - initialX;
             const newY = e.clientY - initialY;
-            setPosition({ x: newX, y: newY });
+            // setPosition({ x: newX, y: newY });
+            // console.log("x: "+newX, "y:"+newY);
+
+            if (newX <= -150) {
+                setPosition({ x: newX, y: newY });
+            }
+
+            const isInArea = (
+                newX >= targetArea.minX && newX <= targetArea.maxX &&
+                newY >= targetArea.minY && newY <= targetArea.maxY 
+            );
+            setIsInTargetArea(isInArea);
         }
         };
 
@@ -23,6 +37,9 @@ export default function Words(){
         setDragging(false);
         window.removeEventListener('mousemove', handleMouseMove);
         window.removeEventListener('mouseup', handleMouseUp);
+        if (isInTargetArea) {
+            console.log('La pieza está en el lugar correcto.');
+        }
         };
 
         window.addEventListener('mousemove', handleMouseMove);
@@ -33,7 +50,7 @@ export default function Words(){
         onMouseDown={handleMouseDown}>
             
             <picture className={styles.containerWord}>
-                <img src={Mentir} className={styles.Word} alt=''/>
+                <img src={image} className={styles.Word} alt=''/>
             </picture>   
             
         </div>
--- src/level3/Level3.jsx
+++ src/level3/Level3.jsx
@@ -4,8 +4,20 @@ import styles from './Level3.module.css';
 import Thermometer from '../img/Thermometer.png';
 import Kitten from '../components/Kitten/Kitten';
 import Words from '../components/Words/Words';
+import Mentir from '../img/Words/Mentir.png';
+import Celar from '../img/Words/Celar.png';
+import Stalkear from '../img/Words/Stalkear.png';
+import Prohibir from '../img/Words/prohibir.png';
+import Golpear from '../img/Words/Golpear.png';
+import Sextorcion from '../img/Words/Sextorcion.png';
+import Amenazar from '../img/Words/Amenazar.png';
+import Violar from '../img/Words/Violar.png';
+import Matar from '../img/Words/Matar.png';
 
 function Level3(){
+    const targetAreas = [
+        {minX: -155, minY: 550, maxX: -150, maxY: 555}
+    ]
     return(
         <div className={styles.Container}>
             <div className={styles.AppHeader}>
@@ -13,11 +25,18 @@ function Level3(){
                 <img src={Hp} className={styles.Hp} alt=''/>
             </div>
             <div className={styles.columnContainer}>
-            <Words/>
+                <Words image={Mentir} targetArea={targetAreas[0]}/>
+                <Words image={Celar} targetArea={targetAreas[0]}/>
+                <Words image={Stalkear} targetArea={targetAreas[0]}/>
+                <Words image={Prohibir} targetArea={targetAreas[0]}/>
+                <Words image={Golpear} targetArea={targetAreas[0]}/>
+                <Words image={Sextorcion} targetArea={targetAreas[0]}/>
+                <Words image={Amenazar} targetArea={targetAreas[0]}/>
+                <Words image={Violar} targetArea={targetAreas[0]}/>
+                <Words image={Matar} targetArea={targetAreas[0]}/>
                 <div className={styles.imgContainer}>
                     <img src={Thermometer} className={styles.Thermometer} alt=''/>
                 </div>
-                
             </div>
             <Kitten/>
         </div>
