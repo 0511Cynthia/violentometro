4d105de7b80e20339e75dc1a6f44dde2154a9177
albertoG-2004 <137020484+albertoG-2004@users.noreply.github.com>
2025-03-25 15:55:41 -0600

    Package realizado, construcción de ejecutable realizada, posible error con rutas relativas

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,10 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import isDev from 'electron-is-dev';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -20,8 +24,6 @@ function createWindow() {
 
     if (isDev) {
         win.webContents.openDevTools();
-    }else{
-        win.loadFile(`${path.join(__dirname, '../build/index.html')}`);
     }
 }
 
